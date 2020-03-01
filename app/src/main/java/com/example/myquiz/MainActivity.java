20fa0bc7d03aa907f264f06f6a0c90f4ab6cd6eb
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-03-01 08:57:53 +0545

    project begins quiz

---
+++ app/src/main/java/com/example/myquiz/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button StartQuiz;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StartQuiz=(Button)findViewById(R.id.StartBtn);
+
+StartQuiz.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(MainActivity.this,QuizActivity.class);
+        startActivity(intent);
+    }
+});
+
+
+    }
+}
