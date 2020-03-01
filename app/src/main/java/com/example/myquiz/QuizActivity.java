505266c498c753d621b7703eb86460f5d4e207cd
Luv Jaiswal <42119313+LuvJaiswal@users.noreply.github.com>
2020-03-01 09:18:19 +0545

    Intents done now for Score activity

--- app/src/main/java/com/example/myquiz/QuizActivity.java
+++ app/src/main/java/com/example/myquiz/QuizActivity.java
@@ -2,13 +2,32 @@ package com.example.myquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button SubmitBtn;
+    private String score;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        SubmitBtn=(Button)findViewById(R.id.submitBtn);
+
+        SubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               score = "10/10";
+                Intent intent = new Intent(QuizActivity.this,ScoreActivity.class);
+                intent.putExtra( "SCORE",score);
+                startActivity(intent);
+            }
+        });
     }
 }
